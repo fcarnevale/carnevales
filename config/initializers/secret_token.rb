bc0e9c0e3641e71272909787d4e5d559b823b3d9
Frank Carnevale <frank.carnevale@gmail.com>
2013-02-08 11:08:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carnevales::Application.config.secret_token = '756d1e7a1a040e97ecd26ff6780c1d3f804dfa792f48b9432650c7043ed414b0b9a1b955e765c00ef8cc35cefcf71de81897f8b03ab8287bfc8ee705b4bc6f67'
