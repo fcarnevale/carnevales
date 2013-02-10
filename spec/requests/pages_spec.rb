15ee44e3d667a22a90b6d397750272ddbf653c95
Frank Carnevale <frank.carnevale@gmail.com>
2013-02-09 20:27:22 -0500

    Setup site layout and implement twitter bootstrap

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,17 +5,17 @@ describe "Pages" do
   describe "Home page" do
 
     it "should have the content 'Carneval.es'" do
-      visit '/pages/home'
-      page.should have_selector('h1', text: 'Carneval.es')
+      visit root_path
+      page.should have_selector('h1', text: 'Calling all Carnevales.')
     end
 
     it "should have the base title" do
-      visit '/pages/home'
+      visit root_path
       page.should have_selector('title', text: "Carneval.es")
 	  end
 
     it "should not have a custom page title" do
-      visit '/pages/home'
+      visit root_path
       page.should_not have_selector('title', text: " [home]")
     end
 
@@ -24,26 +24,57 @@ describe "Pages" do
   describe "Profile page" do
 
     it "should have the content 'My Profile'" do
-      visit '/pages/profile'
+      visit profile_path
       page.should have_selector('h1', text: 'My Profile')
     end
 
     it "should have the title 'Carneval.es [profile]'" do
-      visit '/pages/profile'
-      page.should have_selector('title', text: "Carneval.es [profile]")
+      visit profile_path
+      page.should have_selector('title', text: full_title("profile"))
     end
   end
 
   describe "About page" do
 
     it "should have the content 'About'" do
-      visit '/pages/about'
+      visit about_path
       page.should have_selector('h1', text: 'About')
     end
 
     it "should have the title 'Carneval.es [about]'" do
-      visit '/pages/about'
-      page.should have_selector('title', text: "Carneval.es [about]")
+      visit about_path
+      page.should have_selector('title', text: full_title("about"))
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Carneval.es [contact]'" do
+      visit contact_path
+      page.should have_selector('title', text: full_title("contact"))
+    end
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: "Carneval.es [about]"
+    click_link "Profile"
+    page.should have_selector 'title', text: "Carneval.es [profile]"
+    click_link "Contact"
+    page.should have_selector 'title', text: "Carneval.es [contact]"
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: "Carneval.es [sign up]"
+    click_link "Carnevales"
+    page.should have_selector 'title', text: "Carneval.es" 
+  end
+
+
+
 end
